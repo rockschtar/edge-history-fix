e574595feaf1837350a622c0798240551f62dd06
Stefan Helmer <rockschtar@pm.me>
2024-04-05 19:52:07 +0200

    check

--- src/index.js
+++ src/index.js
@@ -10,3 +10,25 @@ chrome.action.onClicked.addListener((tab) => {
 chrome.commands.onCommand.addListener((command) => {
     openHistoryFix()
 });
+
+chrome.runtime.onInstalled.addListener((details) => {
+    if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+        checkCommandShortcuts();
+    }
+});
+
+function checkCommandShortcuts() {
+    chrome.commands.getAll((commands) => {
+        let missingShortcuts = [];
+
+        for (let {name, shortcut} of commands) {
+            if (shortcut === '') {
+                missingShortcuts.push(name);
+            }
+        }
+
+        if (missingShortcuts.length > 0) {
+            console.log("mi", missingShortcuts)
+        }
+    });
+}
